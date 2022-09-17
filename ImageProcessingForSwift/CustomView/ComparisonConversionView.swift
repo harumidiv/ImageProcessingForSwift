70a3f9a7b4f5465dfc2627e6ee6d4c31b72b084b
佐川晴海[YUMEMI]/Sagawa Harumi <h_sagawa@yumemi.co.jp>
2022-09-17 21:11:05 +0900

    共通で使用するViewを追加しました

--- ImageProcessingForSwift/CustomView/ComparisonConversionView.swift
+++ ImageProcessingForSwift/CustomView/ComparisonConversionView.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ComparisonConversionView: UIView {
-
-    @IBOutlet weak var beforeImage: UIView!
-    @IBOutlet weak var afterImage: UIView!
     
+    @IBOutlet private weak var beforeImage: UIImageView!
+    @IBOutlet private weak var afterImage: UIImageView!
     var tapHandler:(()->Void)?
     
+    // MARK: - Initializer
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadNib()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        loadNib()
+    }
+    
+    func loadNib() {
+        let view = Bundle.main.loadNibNamed("ComparisonConversionView",
+                                            owner: self,
+                                            options: nil)?.first as! UIView
+        view.frame = self.bounds
+        self.addSubview(view)
+    }
+    
+    // MARK: - Action
     @IBAction func tapAction(_ sender: Any) {
         tapHandler?()
     }
--- ImageProcessingForSwift/ViewController.swift
+++ ImageProcessingForSwift/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var hogeView: ComparisonConversionView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
