8457449719c1c63992e1a86433051f45c6d39201
佐川晴海[YUMEMI]/Sagawa Harumi <h_sagawa@yumemi.co.jp>
2022-09-17 21:37:58 +0900

    フォルダを整理しました

--- ImageProcessingForSwift/ViewController.swift
+++ ImageProcessingForSwift/ViewController.swift
@@ -7,15 +7,6 @@
 
 import UIKit
 
-extension UIViewController {
-    static func loadFromNib() -> Self {
-        func instantiateFromNib<T: UIViewController>() -> T {
-            return T.init(nibName: String(describing: T.self), bundle: nil)
-        }
-        return instantiateFromNib()
-    }
-}
-
 final class ViewController: UIViewController {
     struct ImageProcessingData {
         let viewController: UIViewController
